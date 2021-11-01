b20b3e71e695f077da09c7d6521cbb082065fccf
Nikola Ivanov <nikola_wd@hotmail.com>
2021-11-01 14:21:06 +0100

    Add stack data structure

---
+++ data-structures/1-Stack.js
@@ -0,0 +1,89 @@
+// last in, first out
+
+// An arary is already a stack
+
+// last in, first out
+
+// An arary is already a stack
+
+// This is the implementation from scratch
+var Stack = function () {
+  this.count = 0;
+  this.storage = {};
+
+  this.push = (value) => {
+    this.storage[this.count] = value;
+    this.count++;
+  };
+
+  // Removes and returns the value at the end of the stack
+  this.pop = () => {
+    if (this.count === 0) {
+      return undefined;
+    }
+
+    this.count--;
+    var result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  };
+
+  // Returns the value at the end of the stack
+  this.peek = (value) => {
+    return this.storage[this.count - 1];
+  };
+
+  this.size = () => {
+    return this.count;
+  };
+};
+
+var myStack = new Stack();
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
+
+console.log('--------------------------------------------------------');
+
+// This is the implementation from scratch
+class ClassStack {
+  constructor() {
+    this.count = 0;
+    this.storage = {};
+
+    this.push = (value) => {
+      this.storage[this.count] = value;
+      this.count++;
+    };
+
+    // Removes and returns the value at the end of the stack
+    this.pop = () => {
+      if (this.count === 0) {
+        return undefined;
+      }
+
+      this.count--;
+      var result = this.storage[this.count];
+      delete this.storage[this.count];
+      return result;
+    };
+
+    // Returns the value at the end of the stack
+    this.peek = (value) => {
+      return this.storage[this.count - 1];
+    };
+
+    this.size = () => {
+      return this.count;
+    };
+  }
+}
+
+var myStack = new ClassStack();
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
